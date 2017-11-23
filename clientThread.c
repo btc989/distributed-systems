c7a690ff2c9e9f550e5c52e392f9f569ec7a0e38
btc989 <bclar787@gmail.com>
2017-11-23 10:44:39 -0600

    Add files via upload

---
+++ clientThread.c
@@ -0,0 +1,256 @@
+#include "clientServerThreads.h"
+
+void *client_thread (void *args)
+{
+    int socket_fd;
+    struct sockaddr_in server_addr;
+    struct hostent *hp;
+    char host_name [MAX_LINE_SIZE];
+    int host_port_no;
+    struct server_address server_table [MAX_SERVERS];
+    int server_count;
+    char send_line [MAX_LINE_SIZE];
+    char work_c_string [MAX_LINE_SIZE];
+    char caller [MAX_LINE_SIZE];
+    int i;
+    int n;
+
+    gethostname (host_name, sizeof (host_name));
+    sprintf (caller, "    Client [%s]", host_name);
+    printf ("    Client [HOST=%s PID=%d]: ready\n", host_name, getpid ());
+
+    /* Don't start until server is ready */
+    while (SERVER_NOT_READY)
+    {
+        mutex_lock (caller);
+        if (*my_server_ready)
+        {
+            break;
+        }
+        mutex_unlock (caller);
+    }
+    /* Don't start until client is ready */
+    server_count = get_server_addresses (server_table);
+    for (i = 0; i < server_count; i ++)
+    {
+        if (strcmp (host_name, server_table [i].host_name) == 0)
+        {
+            host_port_no = server_table [i].port_no;
+            break;
+        }
+    }
+    mutex_unlock (caller);
+
+    srand (host_port_no);
+
+    while (LOOP_FOREVER)
+    {
+        printf ("    Client [HOST=%s PID=%d]: ENTERING NON-CRITICAL SECTION\n", host_name, getpid ());
+        usleep (2000 * (1 + rand () % 1000));
+        printf ("    Client [HOST=%s PID=%d]: LEAVING NON-CRITICAL SECTION\n", host_name, getpid ());
+
+        /* THE REQUEST PART OF YOUR PRE-PROTOCOL CODE GOES HERE! */
+        /*ADDDED */
+        
+        //create request to be sent
+        strcpy(send_line, "request ");
+        strcat(send_line, host_name);
+        strcat(send_line, " ");
+        
+        sprintf (work_c_string, "%d", host_port_no);
+        strcat(send_line, work_c_string);
+
+        sprintf (work_c_string, "%d", my_ticket_no);
+        strcat(send_line, work_c_string);
+        strcat(send_line, "\0");
+
+        //loop through servers sending out request to all
+        //except own server
+        mutex_lock (caller);
+        for (i = 0; i < server_count; i ++)
+        {
+            if (strcmp (host_name, server_table [i].host_name) != 0)
+            {
+                
+                //open socket
+                //connect to remote server
+
+                if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                    printf("socket ERROR in main");
+                    exit(1);
+                }
+
+                memset( & server_addr, 0, sizeof(server_addr));
+                server_addr.sin_family = AF_INET;
+                hp = server_table [i].host_name;
+                if (hp == (struct hostent * ) NULL) {
+                    printf("gethostbyname ERROR in main: %s does not exist", server_table [i].host_name);
+                    exit(1);
+                }
+                memcpy( & server_addr.sin_addr, hp -> h_addr, hp -> h_length);
+                server_addr.sin_port = server_table [i].port_no;
+
+                if (connect(socket_fd, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0) {
+                    printf("connect ERROR in main");
+                    exit(1);
+                }
+
+                //send request message
+                n = strlen(send_line);
+                if ((i = write_n(socket_fd, send_line, n)) != n) {
+                    printf("ERROR: could not send to server");
+                    exit(1);
+                }
+                else
+                    printf("Request Sent to server %s",server_table [i].host_name);
+
+
+                //close socket
+                close(socket_fd);
+            }
+        }
+        mutex_unlock (caller);
+
+        /*END OF ADDED*/
+
+        printf ("    Client [HOST=%s PID=%d]: WAITING TO ENTER CRITICAL SECTION\n", host_name, getpid ());
+        while (AWAIT_REPLIES)
+        {
+
+            /* THE AWAIT REPLIES PART OF YOUR PRE-PROTOCOL CODE GOES HERE! */
+            /*ADDED*/
+            mutex_lock (caller); 
+            //check shared variable
+            if(*my_replies==server_count){
+                //AWAIT_REPLIES=1;
+                *my_replies=0;
+                break;
+            }
+                
+            mutex_unlock (caller);
+            /*END OF ADDED */
+
+        }
+        printf ("    Client [HOST=%s PID=%d]: ENTERING CRITICAL SECTION\n", host_name, getpid ());
+        write_to_history_file (host_name, host_port_no, my_ticket_no);
+        printf ("    Client [HOST=%s PID=%d]: LEAVING CRITICAL SECTION\n", host_name, getpid ());
+
+        /* THE DEFERRED REPLIES OF YOUR POST-PROTOCOL CODE GOES HERE! */
+        /*ADDED */
+//create request to be sent
+        mutex_lock (caller);
+        my_deferred_count = get_server_addresses (my_deferred_table);
+        for (i = 0; i < my_deferred_count; i ++)
+        {
+            strcpy(send_line, "reply ");
+            strcat(send_line, host_name);
+            strcat(send_line, " ");
+            
+            sprintf (work_c_string, "%d", *my_deferred_table.port_no[i]);
+            strcat(send_line, work_c_string);
+            strcat(send_line, "\0");
+
+            //open socket
+                //connect to remote server
+
+                if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                    printf("socket ERROR in main");
+                    exit(1);
+                }
+
+                memset( & server_addr, 0, sizeof(server_addr));
+                server_addr.sin_family = AF_INET;
+                hp = my_deferred_table[i].host_name;
+                if (hp == (struct hostent * ) NULL) {
+                    printf("gethostbyname ERROR in main: %s does not exist", server_table [i].host_name);
+                    exit(1);
+                }
+                memcpy( & server_addr.sin_addr, hp -> h_addr, hp -> h_length);
+                server_addr.sin_port = my_deferred_table[i].port_no;
+
+                if (connect(socket_fd, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0) {
+                    printf("connect ERROR in main");
+                    exit(1);
+                }
+
+                //send request message
+                n = strlen(send_line);
+                if ((i = write_n(socket_fd, send_line, n)) != n) {
+                    printf("ERROR: could not send to server");
+                    exit(1);
+                }
+                else
+                    printf("Reply Sent to  defered server %s",my_deferred_table[i].host_name);
+                    
+                //close socket
+                close(socket_fd);
+        }
+        my_deferred_count = 0;
+        mutex_unlock (caller);
+        
+        /*END OF ADDED*/
+    }
+
+    exit (0);
+}
+
+int get_server_addresses (struct server_address server_table [])
+{
+    FILE *server_address_file;
+    char char_port_no [MAX_LINE_SIZE];
+    int int_port_no;
+    int server_count = 0;
+    int lock;
+    struct flock key;
+
+    key.l_type = F_WRLCK;
+    key.l_whence = SEEK_SET;
+    key.l_start = 0;
+    key.l_len = 0;
+    key.l_pid = getpid ();
+    lock = open ("server_address_file_lock", O_WRONLY);
+    fcntl (lock, F_SETLKW, &key);
+
+    server_address_file = fopen ("serverAddressFile", "r");
+    if (server_address_file == (FILE *) NULL)
+    {
+        printf ("    Client: fopen failed for serverAddressFile read");
+        exit (1);
+    }
+
+    while (fscanf (server_address_file, "%s", server_table [server_count].host_name) == 1)
+    {
+        fscanf (server_address_file, "%s", char_port_no);
+        int_port_no = atoi (char_port_no);
+        server_table [server_count].port_no = int_port_no;
+        server_count ++;
+    }
+
+    fclose (server_address_file);
+
+    key.l_type = F_UNLCK;
+    fcntl (lock, F_SETLK, &key);
+    close (lock);
+
+    return (server_count);
+}
+
+void write_to_history_file (char host_name [], int host_port_no, int *my_ticket_no)
+{
+    FILE *history_file;
+
+    history_file = fopen ("historyFile", "a");
+    if (history_file == (FILE *) NULL)
+    {
+        printf ("    Client: fopen failed for historyFile append");
+        exit (1);
+    }
+
+    fprintf (history_file, "%s\n", host_name);
+    fprintf (history_file, "%d\n", host_port_no);
+    fprintf (history_file, "%d\n\n", *my_ticket_no);
+
+    fclose (history_file);
+
+    return;
+}
---
+++ serverThread.c
@@ -0,0 +1,261 @@
+#include "clientServerThreads.h"
+
+void *server_thread (void *args)
+{
+    int socket_fd;
+    int new_socket_fd;
+    int addr_length;
+    int child_pid;
+    int child_status;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    struct hostent *hp;
+    char host_name [MAX_LINE_SIZE];
+    int source_socket_fd;
+    struct sockaddr_in source_addr;
+    char message_type [MAX_LINE_SIZE];
+    char source_host_name [MAX_LINE_SIZE];
+    char source_port_no [MAX_LINE_SIZE];
+    char source_ticket_no [MAX_LINE_SIZE];
+    char recv_line [MAX_LINE_SIZE];
+    char send_line [MAX_LINE_SIZE];
+    char work_c_string [MAX_LINE_SIZE];
+    char caller [MAX_LINE_SIZE];
+    int n;
+    int i;
+    mutex_lock(caller);
+    my_deferred_table = (struct server_address *)malloc(MAX_SERVERS);
+    mutex_unlock(caller);
+    if ((socket_fd = socket (AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf ("Server: socket failed");
+        exit (1);
+    }
+
+    memset (&server_addr, 0, sizeof (server_addr));
+    server_addr.sin_family = AF_INET;
+    gethostname (host_name, sizeof (host_name));
+    hp = gethostbyname (host_name);
+    if (hp == (struct hostent *) NULL)
+    {
+        printf ("Server: gethostbyname failed (%s does not exist)", host_name);
+        exit (1);
+    }
+
+    sprintf (caller, "Server [%s]", host_name);
+    memcpy (&server_addr.sin_addr, hp -> h_addr, hp -> h_length);
+    server_addr.sin_port = htons (0);
+    if (bind (socket_fd, (struct sockaddr *) &server_addr, sizeof (server_addr)) < 0)
+    {
+        printf ("Server: bind failed");
+        exit (1);
+    }
+
+    addr_length = sizeof (server_addr);
+    if (getsockname (socket_fd, (struct sockaddr *) &server_addr, &addr_length) < 0)
+    {
+        printf ("Server: getsockname failed");
+        exit (1);
+    }
+
+    if (listen (socket_fd, 128) < 0)
+    {
+        printf ("Server: listen failed");
+        exit (1);
+    }
+    publish_server_address (host_name, ntohs (server_addr.sin_port));
+    printf ("Server [HOST=%s PID=%d]: ready and listening on [PORT=%d]\n", host_name, getpid (), ntohs (server_addr.sin_port));
+
+    while (LOOP_FOREVER)
+    {
+        addr_length = sizeof (client_addr);
+        new_socket_fd = accept (socket_fd, (struct sockaddr *) &client_addr, &addr_length);
+        if (new_socket_fd < 0)
+        {
+            printf ("Server: accept failed");
+            exit (1);
+        }
+        mutex_lock (caller);
+        get_message (new_socket_fd, recv_line, "Server");
+        close (new_socket_fd);
+        parse_message (recv_line, message_type, source_host_name, source_port_no, source_ticket_no);
+        printf ("Server [HOST=%s PID=%d]: received [%s]\n", host_name, getpid (), recv_line);
+        mutex_unlock (caller);
+        if (strcmp (message_type, "start") == 0)
+        {
+            mutex_lock (caller);
+            *my_server_ready = 1;
+            mutex_unlock (caller);
+            continue;
+        }
+        else if (strcmp (message_type, "shutdown") == 0)
+        {
+            close (socket_fd);
+            return ((void *) NULL);
+        }
+        if ((child_pid = fork ()) < 0)
+        {
+            printf ("Server: fork failed");
+            exit (1);
+        }
+        else if (child_pid == 0)
+        {
+
+            /* YOUR REQUEST, REPLY, AND DEFERRED REPLY CODE GOES HERE! */
+            /*ADDED*/
+            //if request
+            if(strcmp (message_type, "request") == 0){
+                int sourceTicketNo = atoi (source_ticket_no);
+                mutex_lock (caller);
+                if(!my_server_ready || (sourceTicketNo < *my_ticket_no)||((sourceTicketNo == my_ticket_no))){ //&& (sourceID < myID ()))
+                    //send reply
+                    strcpy(send_line, "reply ");
+                    strcat(send_line, host_name);
+                    strcat(send_line, " ");
+                    
+                    sprintf (work_c_string, "%d", server_addr.sin_port);
+                    strcat(send_line, work_c_string);
+
+                    strcat(send_line, " ");
+                    sprintf (work_c_string, "%d", *(int*)my_highest_ticket_no);
+                    strcat(send_line, work_c_string);
+                    strcat(send_line, "\0");
+
+                    //open socket
+                        //connect to remote server
+                    /*
+                        if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                            printf("socket ERROR in main");
+                            exit(1);
+                        }
+
+                        memset( & server_addr, 0, sizeof(server_addr));
+                        server_addr.sin_family = AF_INET;
+                        hp = source_host_name;
+                        if (hp == (struct hostent * ) NULL) {
+                            printf("gethostbyname ERROR in main: %s does not exist", server_table [i].host_name);
+                            exit(1);
+                        }
+                        memcpy( & server_addr.sin_addr, hp -> h_addr, hp -> h_length);
+                        server_addr.sin_port = source_port_no;
+
+                        if (connect(socket_fd, (struct sockaddr * ) & server_addr, sizeof(server_addr)) < 0) {
+                            printf("connect ERROR in main");
+                            exit(1);
+                        }*/
+
+                        //send request message
+                        n = strlen(send_line);
+                        if ((i = write_n(socket_fd, send_line, n)) != n) {
+                            printf("ERROR: could not send to server");
+                            exit(1);
+                        }
+                        else
+                            printf("Reply Sent to server %s",source_host_name);
+
+                        //close socket
+                        //close(socket_fd);
+                }
+                else{
+                    //defer reply
+                    *my_deferred_count = *my_deferred_count +1;
+                    *my_deferred_table.host_name = source_host_name;
+                    *my_deferred_table.port_no =source_port_no;
+                }
+                mutex_unlock (caller);
+            }
+            //if a reply
+            else if(strcmp (message_type, "repy") == 0)
+            {
+                mutex_lock (caller);
+                *my_replies = *my_replies +1;
+                mutex_unlock (caller);
+                
+                printf("Reply recieved from server %s",source_host_name);
+            }
+
+            /*END OF ADDED*/
+
+
+        }
+        do
+        {
+            child_pid = waitpid (-1, &child_status, WNOHANG);
+        } while (child_pid > 0);
+    }
+
+    exit (0);
+}
+
+void publish_server_address (char host_name [], int port_no)
+{
+    FILE *server_address_file;
+    int lock;
+    struct flock key;
+
+    key.l_type = F_WRLCK;
+    key.l_whence = SEEK_SET;
+    key.l_start = 0;
+    key.l_len = 0;
+    key.l_pid = getpid ();
+    lock = open ("server_address_file_lock", O_WRONLY);
+    fcntl (lock, F_SETLKW, &key);
+
+    server_address_file = fopen ("serverAddressFile", "a");
+    if (server_address_file == (FILE *) NULL)
+    {
+        printf ("Server: fopen failed for serverAddressFile append");
+        exit (1);
+    }
+
+    fprintf (server_address_file, "%s\n", host_name);
+    fprintf (server_address_file, "%d\n", port_no);
+
+    fclose (server_address_file);
+
+    key.l_type = F_UNLCK;
+    fcntl (lock, F_SETLK, &key);
+    close (lock);
+
+    return;
+}
+
+void parse_message (char recv_line [], char message_type [], char source_host_name [], char source_port_no [], char source_ticket_no [])
+{
+    char message [MAX_LINE_SIZE];
+    char tokens [MAX_TOKENS][MAX_LINE_SIZE];
+
+    strcpy (message, recv_line);
+    tokenize_message (tokens, message);
+
+    strcpy (message_type, tokens [0]);
+    strcpy (source_host_name, tokens [1]);
+    strcpy (source_port_no, tokens [2]);
+    strcpy (source_ticket_no, tokens [3]);
+
+    return;
+}
+
+int tokenize_message (char tokens [][MAX_LINE_SIZE], char message [])
+{
+    char *token;
+    int i;
+
+    for (i = 0; i < MAX_TOKENS; i ++)
+    {
+        strcpy (tokens [i], "");
+    }
+
+    if ((token = strtok (message, " ")) != NULL)
+    {
+        strcpy (tokens [0], token);
+        i = 1;
+        while ((token = strtok (NULL, " ")) != NULL)
+        {
+            strcpy (tokens [i], token);
+            i ++;
+        }
+    }
+
+    return i;
+}
